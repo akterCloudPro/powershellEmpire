9f18fea737a27ecf2cd59bd06d8d68244c271f50
akterCloudPro <73134659+akterCloudPro@users.noreply.github.com>
2023-11-27 23:38:54 +0600

    Function rewrites

--- function-junction/Remove-RegistryKey.ps1
+++ function-junction/Remove-RegistryKey.ps1
@@ -1,30 +1,19 @@
-function Remove-RegistryKey {
+## Function :: Remove targeted registry key items
+function RemoveRegistryKey {
     param (
         [string]$appId,
-        [string]$appVersion,
-        [string]$regChildPath
+        [string]$registryPath
     )
 
-    $targetAppID = "${appId}_${appVersion}"
-    [string[]]$registryHives = @("HKLM:")
-
-    foreach ($hive in $registryHives) {
-        $registryPath = Join-Path -Path $hive -ChildPath ${regChildPath}${targetAppID}
-
-        if (Test-Path -Path $registryPath) {
-            Remove-Item -Path $registryPath -Recurse -Force
-            Write-Host "Registry key '$registryPath' deleted successfully."
-        } else {
-            Write-Host "Registry key '$registryPath' not found."
+    Get-ChildItem $registryPath -Rec -EA SilentlyContinue | ForEach-Object {
+        $CurrentKey = (Get-ItemProperty -Path $_.PsPath)
+        If ($CurrentKey -match $appId){
+          $CurrentKey|Remove-Item -Recurse -Force
         }
-    }
+     }
 }
 
-## Initialize the variables with the necessary values.
-$appID = '010a1e71-f77d-4cc2-b7ec-8d655b6f98a0'
-$appVersion = '1'
-$regChildPath = 'SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\*\'
-
-
-## Usage example:
-Remove-RegistryKey -appId $appID -appVersion $appVersion -regChildPath $regChildPath
+## Example Usage ##
+$appId = '010a1e71-f77d-4cc2-b7ec-8d655b6f98a0' ## Deployed appId from Intune portal 
+$registryPath = 'HKLM:SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\*\'
+RemoveRegistryKey -appId $appId -registryPath $registryPath
